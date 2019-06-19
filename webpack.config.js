70e1c3a3dd45bd9457d734379406a203d78fd31b
roy_wang <roy_wang@lannerinc.com>
2019-06-19 14:40:13 +0800

    [Modify] correction webpack config add image loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,35 +3,37 @@ const path = require('path');
 
 
 module.exports = {
+	mode: 'development',
 	entry: './src/index.js',
 	output:{
 		path: path.resolve(__dirname,'dist'),
 		filename: 'bundle.js'
-	}
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
-          }
-        ]
-      },
-	  { test: /\.css$/, loader: "style-loader!css-loader" }
-    ]
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader"
+				}
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: "html-loader"
+					}
+				]
+			},
+			{ test: /\.css$/, loader: "style-loader!css-loader" },
+			{ test: /\.(jpge|gif|jpg|png)$/, loader:"file-loader"}
+		]
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: "./public/index.html",
-      filename: "./dist/index.html"
+      filename: "index.html"
     })
   ]
 };
