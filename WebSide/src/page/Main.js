c941a3eb6b82dc896f93b9516efb9f50795e882d
roy <mynameisroy89@gmail.com>
2019-07-11 00:37:59 +0800

    [Modify] Add main page content

--- WebSide/src/page/Main.js
+++ WebSide/src/page/Main.js
@@ -3,11 +3,23 @@ import KeyP  from '../image/keyboard.jpg';
 
 function Main(){
     return(
-        <div>
+
+        <div class="MainPage">
             <div class="MyPicture">
-                <img src={KeyP}></img>
+                 <img src={KeyP}></img>
             </div>
+            <div class="MinDes">
+                <p>
+                    Hi~<br />
+                    這個靜態網站<br />
+                    可以看到我在104上可以看到的東西<br />
+                    如果貴公司願意給我個面試的機會<br />
+                    可以再104找到我的連絡方式<br />
+                </p>
+            </div>
+
         </div>
+
     );
 }
 
